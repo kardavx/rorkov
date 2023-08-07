254fe7b8d94095fd39114f48fe06fa64e16d3b4d
Krzymen <krzymen.kontakt@gmail.com>
2023-08-07 22:42:02 +0200

    npm run devbuild + aktualizacja readme

--- src/client/controllers/test.ts
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@flamework/core";
-import { OnCharacterAdded } from "./core";
-
-@Controller({})
-export class Test implements OnCharacterAdded {
-	onCharacterAdded(character: Model): void {
-		print(`${character.Name} added!`);
-	}
-}
