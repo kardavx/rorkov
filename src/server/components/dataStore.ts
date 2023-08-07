1c85aa10f656ee85bcbd419b8c2455f1b455be93
Krzymen <krzymen.kontakt@gmail.com>
2023-08-07 21:50:26 +0200

    aktualizacja project.json

--- src/server/components/dataStore.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, OnStart } from "@flamework/core";
-import { DataStoreService } from "@rbxts/services";
-import { Players } from "@rbxts/services";
-
-const dataStore = DataStoreService.GetDataStore("GameData");
-
-@Controller({})
-export class DataStore {}
