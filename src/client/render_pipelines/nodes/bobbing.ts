36bf148e391ff4e7b91753bdbdb935ba96702ad5
T34P07 <t34p07d3v@gmail.com>
2023-08-10 20:08:33 +0200

    elo elo 320

--- src/client/render_pipelines/nodes/bobbing.ts
+++ src/client/render_pipelines/nodes/bobbing.ts
@@ -1,13 +1,33 @@
+import { Controller } from "@flamework/core";
 import { Sine } from "shared/math_utility";
+import { OnCharacterAdded } from "client/controllers/core";
 import { Node } from "../node";
 
+@Controller({})
 export class Bobbing implements Node {
-	private xSine = new Sine(1, 1, 0);
-	private ySine = new Sine(1, 1, 0);
-	private zSine = new Sine(1, 1, 0);
+	private amplitude = 0.5;
+	private frequency = 5;
+	private phase = 0;
 
-	preUpdate(deltaTime: number, playerVelocity: number): void {}
+	private sines = [new Sine(1, 1, 0), new Sine(1, 1, 0)];
+
+	private bobbingAmount: Vector3 = new Vector3();
+
+	getSin(amplitude: number, frequency: number, phase: number) {
+		return amplitude * math.sin(os.clock() * frequency + phase) * 0.1;
+	}
+
+	preUpdate(deltaTime: number, playerVelocity: number): void {
+		this.sines.forEach((sine: Sine) => {
+			sine.setAmplitude(this.amplitude);
+			sine.setFrequency(this.frequency);
+		});
+
+		this.bobbingAmount = new Vector3(this.sines[0].update(), 0, this.sines[1].update());
+	}
+
 	update(deltaTime: number, currentCFrame: CFrame, playerVelocity: number): CFrame {
-		return currentCFrame.mul(CFrame.Angles(this.xSine.update(), this.ySine.update(), this.zSine.update()));
+		return currentCFrame.add(this.bobbingAmount);
+		//return currentCFrame.mul(CFrame.Angles(this.xSine.update(), this.ySine.update(), this.zSine.update()));
 	}
 }
