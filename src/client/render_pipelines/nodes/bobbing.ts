4e6d4e01e989684739b34a7be4c6f122222b66c6
plebs <kardaszjanojutubero@gmail.com>
2023-08-10 18:20:10 +0200

    Poprawki

--- src/client/render_pipelines/nodes/bobbing.ts
+++ src/client/render_pipelines/nodes/bobbing.ts
@@ -1,15 +1,13 @@
 import { Controller } from "@flamework/core";
 import { Sine } from "shared/math_utility";
-import { OnCharacterAdded } from "client/controllers/core";
 import { Node } from "../node";
 
 @Controller({})
 export class Bobbing implements Node {
-	private amplitude = 0.5;
-	private frequency = 5;
-	private phase = 0;
-
-	private sines = [new Sine(1, 1, 0), new Sine(1, 1, 0)];
+	private sines = {
+		X: new Sine(0.5, 5, 0),
+		Z: new Sine(0.5, 5, 0),
+	};
 
 	private bobbingAmount: Vector3 = new Vector3();
 
@@ -18,12 +16,7 @@ export class Bobbing implements Node {
 	}
 
 	preUpdate(deltaTime: number, playerVelocity: number): void {
-		this.sines.forEach((sine: Sine) => {
-			sine.setAmplitude(this.amplitude);
-			sine.setFrequency(this.frequency);
-		});
-
-		this.bobbingAmount = new Vector3(this.sines[0].update(), 0, this.sines[1].update());
+		this.bobbingAmount = new Vector3(this.sines.X.update(), 0, this.sines.Z.update());
 	}
 
 	update(deltaTime: number, currentCFrame: CFrame, playerVelocity: number): CFrame {
