c4c53543f1152b6d4a21cce4c85d78818031bfdf
T34P07 <t34p07d3v@gmail.com>
2023-08-11 13:02:47 +0200

    wqe

--- src/client/render_pipelines/nodes/bobbing.ts
+++ src/client/render_pipelines/nodes/bobbing.ts
@@ -1,6 +1,7 @@
 import { Controller } from "@flamework/core";
 import { Sine } from "shared/math_utility";
 import { Node } from "../node";
+import { EquippedItem } from "client/types/items";
 
 @Controller({})
 export class Bobbing implements Node {
@@ -18,16 +19,17 @@ export class Bobbing implements Node {
 	// };
 
 	private sines = {
-		zOrientation: new Sine(1.3, 1, 0),
-		yOrientation: new Sine(0.6, 1, 0),
-		y: new Sine(1.2, 2, 0),
-		x: new Sine(1.3, 1, 0),
-		z: new Sine(1.4, 2, 0),
+		x: new Sine(0, 2, 0.2),
+		y: new Sine(0.015, 3, 0.3),
+		z: new Sine(0.05, 2, 0.1),
+		pitch: new Sine(0.1, 2, 0.25),
+		yaw: new Sine(0.015, 1, 0.5),
+		roll: new Sine(0.5, 1.5, 0.8),
 	};
 
 	private bobbingAmount: CFrame = new CFrame();
 
-	preUpdate(deltaTime: number, playerVelocity: number): void {
+	preUpdate(deltaTime: number, playerVelocity: number, camCF: CFrame, equippedItem: EquippedItem): void {
 		// this.sines.X.setFrequency((playerVelocity * this.frequency) / 2);
 		// this.sines.X.setAmplitude((this.amplitudes.X * playerVelocity) / 20);
 		// this.sines.Y.setFrequency(playerVelocity * this.frequency * 2);
@@ -39,17 +41,17 @@ export class Bobbing implements Node {
 		// const bobY = this.sines.Y.update();
 		// const bobZ = this.sines.Z.update();
 
-		const zOrientation = this.sines.zOrientation.update();
-		const yOrientation = this.sines.yOrientation.update();
-		const y = this.sines.y.update();
 		const x = this.sines.x.update();
+		const y = this.sines.y.update();
 		const z = this.sines.z.update();
+		const pitch = this.sines.pitch.update();
+		const yaw = this.sines.yaw.update();
+		const roll = this.sines.roll.update();
 
-		this.bobbingAmount = this.bobbingAmount.Lerp(new CFrame(x, y, -z).mul(CFrame.Angles(0, yOrientation, zOrientation)), 5 * deltaTime);
+		this.bobbingAmount = this.bobbingAmount.Lerp(new CFrame(x, y, z).mul(CFrame.Angles(pitch, yaw, roll)), 5 * deltaTime);
 	}
 
-	update(deltaTime: number, currentCFrame: CFrame, playerVelocity: number): CFrame {
-		// Apply the calculated bobbing amount to the player's position
+	update(deltaTime: number, currentCFrame: CFrame, playerVelocity: number, camCF: CFrame, equippedItem: EquippedItem): CFrame {
 		return currentCFrame.mul(this.bobbingAmount);
 	}
 }
