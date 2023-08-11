a56f344ce75fe27fb2c27a103b7a8ee05f2f9268
plebs <pleeeeeeeeebs@gmail.com>
2023-08-11 11:05:40 +0200

    chuj

--- src/client/render_pipelines/nodes/bobbing.ts
+++ src/client/render_pipelines/nodes/bobbing.ts
@@ -5,33 +5,45 @@ import { Node } from "../node";
 @Controller({})
 export class Bobbing implements Node {
 	private frequency = 0.5;
-	private amplitudes = {
-		X: 0.25,
-		Y: 0.1,
-		Z: 1,
-	};
+	// private amplitudes = {
+	// 	X: 0.25,
+	// 	Y: 0.1,
+	// 	Z: 1,
+	// };
+
+	// private sines = {
+	// 	X: new Sine(this.amplitudes.X, this.frequency, 0),
+	// 	Y: new Sine(this.amplitudes.Y, this.frequency, 0),
+	// 	Z: new Sine(this.amplitudes.Z, this.frequency, 0),
+	// };
 
 	private sines = {
-		X: new Sine(this.amplitudes.X, this.frequency, 0),
-		Y: new Sine(this.amplitudes.Y, this.frequency, 0),
-		Z: new Sine(this.amplitudes.Z, this.frequency, 0),
+		zOrientation: new Sine(0.3, 0.5, 0),
+		y: new Sine(0.2, 1, 0),
+		x: new Sine(0.3, 0.5, 0),
+		z: new Sine(0.4, 1, 0),
 	};
 
 	private bobbingAmount: CFrame = new CFrame();
 
 	preUpdate(deltaTime: number, playerVelocity: number): void {
-		this.sines.X.setFrequency((playerVelocity * this.frequency) / 2);
-		this.sines.X.setAmplitude((this.amplitudes.X * playerVelocity) / 20);
-		this.sines.Y.setFrequency(playerVelocity * this.frequency * 2);
-		this.sines.Y.setAmplitude((this.amplitudes.Y * playerVelocity) / 20);
-		this.sines.Z.setFrequency(playerVelocity * this.frequency);
-		this.sines.Z.setAmplitude((this.amplitudes.Z * playerVelocity) / 20);
+		// this.sines.X.setFrequency((playerVelocity * this.frequency) / 2);
+		// this.sines.X.setAmplitude((this.amplitudes.X * playerVelocity) / 20);
+		// this.sines.Y.setFrequency(playerVelocity * this.frequency * 2);
+		// this.sines.Y.setAmplitude((this.amplitudes.Y * playerVelocity) / 20);
+		// this.sines.Z.setFrequency(playerVelocity * this.frequency);
+		// this.sines.Z.setAmplitude((this.amplitudes.Z * playerVelocity) / 20);
 
-		const bobX = this.sines.X.update();
-		const bobY = this.sines.Y.update();
-		const bobZ = this.sines.Z.update();
+		// const bobX = this.sines.X.update();
+		// const bobY = this.sines.Y.update();
+		// const bobZ = this.sines.Z.update();
 
-		this.bobbingAmount = this.bobbingAmount.Lerp(new CFrame(bobX, bobY, bobZ).mul(CFrame.Angles(0, 0, bobZ)), 5 * deltaTime);
+		const zOrientation = this.sines.zOrientation.update();
+		const y = this.sines.y.update();
+		const x = this.sines.x.update();
+		const z = this.sines.z.update();
+
+		this.bobbingAmount = this.bobbingAmount.Lerp(new CFrame(x, y, z).mul(CFrame.Angles(0, 0, zOrientation)), 5 * deltaTime);
 	}
 
 	update(deltaTime: number, currentCFrame: CFrame, playerVelocity: number): CFrame {
