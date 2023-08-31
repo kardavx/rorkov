db01c3f314a7394c124b604fb31b9ad2de829131
plebs <pleeeeeeeeebs@gmail.com>
2023-08-31 10:29:06 +0200

    p0pr4wk1

--- src/client/controllers/camera.ts
+++ src/client/controllers/camera.ts
@@ -9,21 +9,25 @@ type FOVModifiers = { [modifierName in string]: FOVModifier | undefined };
 
 export class Modifier {
 	static modifiers: Modifiers = {};
-	static dampenAmount = 5;
 
-	static create = (name: string, isAutomaticallyDampened = false): Modifier => {
-		if (!Modifier.modifiers[name]) Modifier.modifiers[name] = new Modifier(name, isAutomaticallyDampened);
+	static create = (name: string, isAutomaticallyDampened = false, dampenAmount = 5): Modifier => {
+		if (!Modifier.modifiers[name]) Modifier.modifiers[name] = new Modifier(name, isAutomaticallyDampened, dampenAmount);
 		return Modifier.modifiers[name] as Modifier;
 	};
 
 	static getSummedOffsets = (): CFrame => {
-		let finalOffset = new CFrame();
+		let finalOffset: CFrame | undefined;
 
 		for (const [_, modifierObject] of pairs(Modifier.modifiers)) {
+			if (!finalOffset) {
+				finalOffset = modifierObject.getOffset();
+				continue;
+			}
+
 			finalOffset = finalOffset.mul(modifierObject.getOffset());
 		}
 
-		return finalOffset;
+		return finalOffset || new CFrame();
 	};
 
 	static updateOffsets = (deltaTime: number): void => {
@@ -35,7 +39,7 @@ export class Modifier {
 	private offset: CFrame = new CFrame();
 	private destroyed = false;
 
-	private constructor(private name: string, private isAutomaticallyDampened: boolean) {}
+	private constructor(private name: string, private isAutomaticallyDampened: boolean, private dampenAmount: number) {}
 
 	public getOffset = (): CFrame => {
 		if (this.destroyed) throw `Attempt to get offset of modifier after it was destroyed`;
@@ -53,7 +57,7 @@ export class Modifier {
 		if (this.destroyed) throw `Attempt to update modifier after it was destroyed`;
 
 		if (this.isAutomaticallyDampened) {
-			this.setOffset(this.getOffset().Lerp(new CFrame(), Modifier.dampenAmount * deltaTime));
+			this.setOffset(this.getOffset().Lerp(new CFrame(), this.dampenAmount * deltaTime));
 		}
 	};
 
@@ -120,7 +124,6 @@ export class Camera implements OnPreCameraRender, OnPostCameraRender, OnCharacte
 	private lastOffsets: CFrame = new CFrame();
 	private rotationDelta: Vector2 = new Vector2();
 	private lastCameraCFrame: CFrame | undefined;
-	private rawCamera = new CFrame();
 
 	private applyPosition(summedOffset: CFrame) {
 		const headCF = this.head!.CFrame;
@@ -155,7 +158,7 @@ export class Camera implements OnPreCameraRender, OnPostCameraRender, OnCharacte
 	}
 
 	getRawCFrame(): CFrame {
-		return this.rawCamera;
+		return Camera.camera!.CFrame.mul(this.lastOffsets.Inverse());
 	}
 
 	onCharacterAdded(character: Model): void {
@@ -178,8 +181,6 @@ export class Camera implements OnPreCameraRender, OnPostCameraRender, OnCharacte
 		Modifier.updateOffsets(deltaTime);
 		const summedOffset = Modifier.getSummedOffsets();
 		const fovDifference = FOVModifier.getSummedDifferences();
-
-		this.rawCamera = Camera.camera!.CFrame.mul(summedOffset.Inverse());
 
 		this.applyRotation(summedOffset);
 		if (this.humanoid) this.applyPosition(summedOffset);
--- src/client/controllers/freelook.ts
+++ src/client/controllers/freelook.ts
@@ -6,13 +6,16 @@ import { OnCharacterAdded } from "./core";
 
 @Controller({})
 export class Freelook implements OnStart, OnTick, OnCharacterAdded {
-	constructor(private input: Input) {}
+	static xAxisMax = 1;
+	static yAxisMax = 1;
 
-	private freelookModifier = Modifier.create("freelook", false);
+	private freelookModifier = Modifier.create("freelook", false, 3);
 	private isFreeloking = false;
 	private freelookOffset = Vector2.zero;
 
 	private humanoid: Humanoid | undefined;
+
+	constructor(private input: Input) {}
 
 	onStart(): void {
 		this.input.bindAction(`enableFreelook`, Enum.UserInputType.MouseButton3, 1, (inputState: boolean) => {
@@ -37,6 +40,13 @@ export class Freelook implements OnStart, OnTick, OnCharacterAdded {
 			this.freelookOffset = this.freelookOffset.Lerp(Vector2.zero, 0.1);
 		}
 
-		this.freelookModifier.setOffset(CFrame.Angles(this.freelookOffset.Y, this.freelookOffset.X, 0));
+		this.freelookOffset = new Vector2(
+			math.clamp(this.freelookOffset.X, -Freelook.xAxisMax, Freelook.xAxisMax),
+			math.clamp(this.freelookOffset.Y, -Freelook.yAxisMax, Freelook.yAxisMax),
+		);
+
+		this.freelookModifier.setOffset(
+			new CFrame(0, this.freelookOffset.X / 2, this.freelookOffset.X / 4).mul(CFrame.Angles(this.freelookOffset.Y, this.freelookOffset.X, 0)),
+		);
 	}
 }
